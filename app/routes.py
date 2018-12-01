77cfa92363c36973099307edbc2214b623961390
Siddharth <sidjain1412@gmail.com>
2018-12-02 03:00:56 +0530

    Updated routes.py, with error handling for empty username, added html templating to deal with that, made some visual changes (still ugly af), to be fixed

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,21 @@
-from flask import render_template, request
+from flask import render_template, request, jsonify, redirect
 from app.models import User, Message
 from app import app
 
 
 @app.route('/')
-def index():
-    return render_template('login.html')
+def index(error=None):
+    return render_template('login.html', error=error)
 
 
-@app.route('/take_username', methods=["POST"])
+@app.route('/login', methods=["POST"])
 def take_username():
     try:
         username = request.form['username']
         if username:
             return render_template('chat.html', username=username)
         else:
-            return render_template('login.html')
+            return render_template('login.html',
+                                   error="Please enter a valid username")
     except Exception as e:
         return render_template('login.html')
